99067a1ac2278fa01f1cbb1e4ae53f48a30b9e0d
mohammadhayajneh2014 <https://github.com/mohammadhayajneh2014>
2021-06-13 15:25:41 +0300

    lab 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+class App extends React.Component {
+constructor(props){
+  super(props)
+  this.state={
+data:'',
+messageForWrongInput:'',
+displayErrMsg: false,
+displayMap:false
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  }
 }
 
+gitingcity= async(event)=>{
+event.preventDefault();
+let LocationIQ = event.target.LocationIQ.value;
+let LocationUrl  = `https://eu1.locationiq.com/v1/search.php?key=pk.bc65df9798f7b8653e9f6d9a36f47165&q=${LocationIQ}&format=json`;
+try {
+  let allAboutLocation = await axios.get(LocationUrl);
+  console.log(allAboutLocation.data);
+  this.setState({
+    data:allAboutLocation.data[0],
+    displayMap:true
+  })
+}
+catch {
+  this.setState({
+    messageForWrongInput: 'error this is a bad response',
+    displayErrMsg:true
+  })
+}
+
+
+
+}
+render(){
+return(
+
+<div>
+<form onSubmit={this.gitingcity}>
+<input type='text' placeholder='city name' name='LocationIQ'/>
+<input type='submit' value='yor city'/>
+</form>
+<p>{this.state.data.display_name}</p>
+        <p>{this.state.data.lon}</p>
+        <p>{this.state.data.lat}</p>
+        { this.state.displayErrMsg && this.state.messageForWrongInput}
+        {this.state.displayMap &&  <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.bc65df9798f7b8653e9f6d9a36f47165&center=${this.state.data.lat},${this.state.data.lon}&zoom=18&size=500x500&`} alt='map'/>}
+
+</div>
+);
+}
+}
 export default App;
